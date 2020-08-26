eeadcc389c2d45dfba15e6fb5c34113680c54e04
quannguyen1778 <59248721+quannguyen1778@users.noreply.github.com>
2020-08-26 19:28:46 +0700

    crud speaking, listening

---
+++ app/Http/Controllers/Backend/SpeakingController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Speaking;
+use Illuminate\Http\Request;
+
+class SpeakingController extends Controller
+{
+    public function createSpeaking(Request $r)
+    {
+        $speaking = new  Speaking();
+        $speaking->content = $r->get('content');
+        $speaking->status = 1;
+        $speaking->save();
+        return response()->json($speaking);
+    }
+
+    public function updateSpeaking(Request $r)
+    {
+        $speaking = Speaking::find($r->get("id"));
+        $speaking->content = $r->get('content');
+        $speaking->status = $r->get('status');
+        $speaking->save();
+        return response()->json($speaking);
+    }
+
+    public function readSpeaking(Request $r)
+    {
+        $speaking = Speaking::find($r->get("id"));
+        return response()->json($speaking);
+    }
+}
