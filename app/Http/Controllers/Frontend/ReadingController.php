4093f627c6d59459f002e1db7ecef6e169372a3a
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2020-08-27 17:14:59 +0700

    api reading test, writing test

--- app/Http/Controllers/Frontend/ReadingController.php
+++ app/Http/Controllers/Frontend/ReadingController.php
@@ -7,14 +7,25 @@ use App\Listening;
 use App\ListeningQuestion;
 use App\Reading;
 use App\ReadingQuestion;
+use App\Result;
 use Illuminate\Http\Request;
 
 class ReadingController extends Controller
 {
-        public function reading()
+    public function reading()
     {
-        $reading=Reading::inRandomOrder()->first();
-        $readingQuestion=ReadingQuestion::where('reading_id',$reading->id)->get();
+        $reading = Reading::inRandomOrder()->first();
+        $readingQuestion = ReadingQuestion::where('reading_id', $reading->id)->get();
         return response()->json([$reading, $readingQuestion], 200);
+    }
+
+    public function storeResult(Request $request)
+    {
+        $result = new Result();
+        $result->student_id = $request->input('student_id');
+        $result->type = 1;
+        $result->point = $request->input('point');
+        $result->save();
+        return response()->json($result, 200);
     }
 }
