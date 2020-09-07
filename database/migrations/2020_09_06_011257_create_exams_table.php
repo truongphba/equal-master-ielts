316b1bfaed4b83841e062d6a70f9b85e57ffcc14
truongph-fpt <truongphth1907039@fpt.edu.vn>
2020-09-07 15:25:27 +0700

    truongph

---
+++ database/migrations/2020_09_06_011257_create_exams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('lecture_id');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('lecture_id')->references('id')->on('users');
+            $table->double('money');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
