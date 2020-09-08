c243ca4016323f70556df7a9fb213a6ee864b905
truongphba <truongph.ba@gmail.com>
2020-09-09 00:43:35 +0700

    truongph

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ListeningQuestionSeeder::class);
         $this->call(ListeningSeeder::class);
-        $this->call(NotificationSeeder::class);
+//        $this->call(NotificationSeeder::class);
         $this->call(ReadingQuestionSeeder::class);
         $this->call(ReadingSeeder::class);
-        $this->call(ResultSeeder::class);
+//        $this->call(ResultSeeder::class);
         $this->call(ScheduleSeeder::class);
         $this->call(SpeakingSeeder::class);
         $this->call(WritingAnswerSeeder::class);
