bf83416a87a89e619f1864b0846808b478f63eab
truongph-fpt <truongphth1907039@fpt.edu.vn>
2020-09-13 22:42:11 +0700

    truongph

--- app/Http/Middleware/Cor2.php
+++ app/Http/Middleware/Cor2.php
@@ -19,7 +19,11 @@ class Cor2
         $response->headers->set('Access-Control-Allow-Origin' , '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
-
+        $response->headers->set("Access-Control-Max-Age", "3600");
+        $response->headers->set("Access-Control-Allow-Credentials", "true");
+        $response->headers->set("Access-Control-Expose-Headers", "Authorization");
+        $response->headers->set("Access-Control-Expose-Headers", "responseType");
+        $response->headers->set("Access-Control-Expose-Headers", "observe");
         return $response;
     }
 }
