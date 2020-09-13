d0209420d79a579df4a29d27cf144e46aecff31c
truongph-fpt <truongphth1907039@fpt.edu.vn>
2020-09-13 22:00:22 +0700

    truongph

--- app/Http/Middleware/Cor2.php
+++ app/Http/Middleware/Cor2.php
@@ -15,10 +15,9 @@ class Cor2
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
+        return $next($request);
     }
 }
